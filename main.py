be5b679c2587ddaba4c88049c2dfa29db598a990
mvstermind <zablockidawid1@gmail.com>
2025-01-19 16:37:06 +0100

    feat: we translate homie

---
+++ main.py
@@ -0,0 +1,18 @@
+import argostranslate.package
+import argostranslate.translate
+
+from_code = "pl"
+to_code = "en"
+
+# Download and install Argos Translate package
+argostranslate.package.update_package_index()
+available_packages = argostranslate.package.get_available_packages()
+package_to_install = next(
+    filter(
+        lambda x: x.from_code == from_code and x.to_code == to_code, available_packages
+    )
+)
+argostranslate.package.install_from_path(package_to_install.download())
+
+translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
+print(translatedText)
