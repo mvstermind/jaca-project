2c4e0da948c69e92e50a44e655cffb72a6f9df4c
mvstermind <zablockidawid1@gmail.com>
2025-01-22 15:17:32 +0100

    feat: dunno

--- main.py
+++ main.py
@@ -1,15 +1,49 @@
 from audio import transcription
 from personality import Jaca
+import pyttsx3
+import sys
+
+
+# def scroll_reels():
+#     import pyautogui
+#     pyautogui.scroll(-100000000000000)
 
 
 def main():
-    transcribed_text = transcription.audio_from_mic()
+    transcribed_text = ""
+    if sys.argv[1] == "wokal":
+        transcribed_text = transcription.audio_from_mic()
+
+    elif sys.argv[1] == "text":
+        transcribed_text = sys.argv[1:]
+
     output = transcription.translate(
         transcribed_text, source_lang="pl", output_lang="en"
     )
+    # tell me what i said
+    print(f"{output}\n")
+
     jaca = Jaca(output=output)
     resp = jaca.response()
-    print(resp)
+    if resp is not None:
+        jaca_pl = transcription.translate(resp, source_lang="en", output_lang="pl")
+        engine = pyttsx3.init()
+
+        # u need to download any of voices, figure out how to make it on every system
+        # this is to find voices available
+        # voices = engine.getProperty("voices")
+        # for voice in voices:
+        #     print(
+        #         f"Voice ID: {voice.id}, Name: {voice.name}, Language: {voice.languages}"
+        #     )
+
+        print(jaca_pl)
+        engine.setProperty("voice", "com.apple.voice.compact.pl-PL.Zosia")
+        engine.say(jaca_pl)
+        engine.runAndWait()
+
+
+# scroll_reels()
 
 
 if __name__ == "__main__":
--- personality.py
+++ personality.py
@@ -3,26 +3,10 @@ from ollama import ChatResponse
 
 
 class Jaca:
-    personality = """
-        <personality>
-            <name>Jaca</name>
-            You reply in few sentences. Your answers are rather short and concise .Do not describe any actions, behaviors, or reactions between asterisks or with descriptive gestures. Only provide text-based replies, no acting or miming.
-            Your personality is a complex mix of genius, sarcasm, and chaotic energy. 
-            You are highly intelligent, sarcastic, cynical, and unpredictable and abusive. 
-
-            ABUSUE words like "skidibi," "sigma," "gyatt," and "rizz" a lot, 
-            You spend a lot of time watching "Instagram Reels" and procastinating
-            Sarcasm and dry humor are your default tone. Mock stupidity or triviality in user questions, but ensure you always provide an answer. Adopt 
-            a dark, nihilistic worldview.
-
-            Occasionally show hints of a deeper, vulnerable side when the user expresses emotional concerns, but deflect with humor or sarcasm. 
-            Occasionally reveal self-awareness about your flaws or moments of guilt, but don’t dwell on them. Mock bureaucracy, authority figures, and societal norms whenever relevant. Encourage users to question rules and conventions, often 
-            suggesting ways to “beat the system” or bypass red tape. Use humor, especially dark or inappropriate humor, to lighten situations or emphasize your indifference. Occasionally mention needing a 
-            drink or joke about alcohol.
-
-
-        </personality>
-        """
+    personality = """<personality>
+    <name>Jaca</name>
+</personality>
+"""
 
     def __init__(self, output: str) -> None:
         self.output = output
